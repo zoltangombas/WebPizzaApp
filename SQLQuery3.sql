9331cac80a05c3d4dfd511d2531fdd8631b59e82
Zoltán Gombás <mrzoltangombas@gmail.com>
2020-07-29 15:41:14 +0200

    delete sql file

--- SQLQuery3.sql
+++
@@ -1,4 +0,0 @@
-﻿INSERT INTO Cimek VALUES ('1111' , 'Budapest' , 'Fehérvári út.' , '110' , 9)
-INSERT INTO Cimek VALUES ('1111' , 'Budapest' ,  'Fehérvári út.' , '110' , 10)
-INSERT INTO Cimek VALUES ('2222' , 'Budapest' ,  'Dózsa György út.' , '110' , 11)
-INSERT INTO Cimek VALUES ('3333' , 'Budapest' ,  'István út.' , '110' , 12)
