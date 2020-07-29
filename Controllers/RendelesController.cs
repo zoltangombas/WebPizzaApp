4f1ec65e4bb861e1c587d12b8344f5a7a36a4c10
Zoltán Gombás <mrzoltangombas@gmail.com>
2020-07-29 15:32:50 +0200

    Migrations2

---
+++ Controllers/RendelesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebPizzaApp.Data;
+
+namespace WebPizzaApp.Controllers
+{
+    public class RendelesController : Controller
+    {
+        private readonly WebPizzaAppDbContext _context;
+
+        public RendelesController(WebPizzaAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Rendelesek.ToListAsync());
+        }
+    }
+}
